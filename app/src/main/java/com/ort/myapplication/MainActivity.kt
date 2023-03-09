626ec125582ea13b2d54306a0734e45bd7ccfc04
facu3064 <106401462+Santimeca@users.noreply.github.com>
2023-03-09 14:18:27 -0300

    boton funcional

--- app/src/main/java/com/ort/myapplication/MainActivity.kt
+++ app/src/main/java/com/ort/myapplication/MainActivity.kt
@@ -2,28 +2,26 @@ package com.ort.dap_actinicio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import java.awt.Button
-import java.awt.font.TextLayout
-
-lateinit var label : TEXT1
-lateinit var btnShow : BUTTON1
+import android.widget.Button
+import android.widget.TextView
+import com.ort.myapplication.R
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var label : TEXT1
-    lateinit var btnShow : BUTTON1
+    lateinit var label : TextView
+    lateinit var btnShow : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        lable = findViewById(R.id.TEXT1)
-        btnShow = findViewById(R.id.button)
+        label = findViewById(R.id.TEXT1)
+        btnShow = findViewById(R.id.BUTTON1)
 
-        lable.text = ""
+        label.text = ""
 
         btnShow.setOnClickListener {
-            lable.text = "hace calor"
+            label.text = "hace calor"
         }
     }
 
